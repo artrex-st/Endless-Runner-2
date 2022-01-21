19246be499c40379531d9adbae2e821eeb753f3f
Arthur Stefanelli <arthur-sst@hotmail.com>
2022-01-20 23:18:25 -0300

    edit obstacle spawner player touch threshold

--- Assets/_RunTime/Scripts/Level/ObstacleSpawner.cs
+++ Assets/_RunTime/Scripts/Level/ObstacleSpawner.cs
@@ -12,15 +12,12 @@ public class ObstacleSpawner : MonoBehaviour
 
     public void SpawnObstacle()
     {
-        if (transform.childCount <= 0 || !transform.GetChild(0).gameObject.activeInHierarchy)
-        {
-            Obstacle prefab = _obstaclePrefabOptions[Random.Range(0, _obstaclePrefabOptions.Length)];
-            
-            _currentObstacle = PoolingSystem.Instance.GetObject(prefab.gameObject);
-            _currentObstacle.transform.parent = transform;
-            _currentObstacle.transform.localPosition = Vector3.zero;
-            _currentObstacle.transform.rotation = Quaternion.identity;
-        }
+        Obstacle prefab = _obstaclePrefabOptions[Random.Range(0, _obstaclePrefabOptions.Length)];
+        
+        _currentObstacle = PoolingSystem.Instance.GetObject(prefab.gameObject);
+        _currentObstacle.transform.parent = transform;
+        _currentObstacle.transform.localPosition = Vector3.zero;
+        _currentObstacle.transform.rotation = Quaternion.identity;
     }
     private void OnDrawGizmos()
     {
